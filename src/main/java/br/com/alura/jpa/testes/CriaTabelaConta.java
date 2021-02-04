9158adea9fa4bc4d69cf5bc121301b28d3d6005e
iagoAguiar <iagoaguiar202@gmail.com>
2021-02-04 10:33:00 -0300

    first commit

---
+++ src/main/java/br/com/alura/jpa/testes/CriaTabelaConta.java
@@ -0,0 +1,15 @@
+package br.com.alura.jpa.testes;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class CriaTabelaConta {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
+		emf.close();
+
+	}
+	
+}
